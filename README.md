330f468fd63c338de00c381cba6a56af833ee387
Hu3diger <martinruediger2010@gmail.com>
2019-10-08 15:47:05 -0300

    Added Logs and new checkin pages

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 This project was developed for fun and for the subject of Software Manage and Configuration
 
 Authors of the idea: [Altieri](https://github.com/AltieriR) and [Martin](https://github.com/Hu3diger)
+
 Group: [Altieri](https://github.com/AltieriR), [Martin](https://github.com/Hu3diger), [Crystian](https://github.com/CrystianJordan) and [Mateus](https://github.com/mateusrcm)
 
--- src/app/pages/new-checkin/new-checkin.component.ts
+++ src/app/pages/new-checkin/new-checkin.component.ts
@@ -2,9 +2,29 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-new-checkin',
-    templateUrl: './new-checkin.component.html'
+    templateUrl: './new-checkin.component.html',
+    styleUrls: ['./new-checkin.component.scss']
 })
 
 export class NewCheckinComponent {
+    checkinImage: any;
+    checkoutImage: any;
 
+    onSelectFile(event: any, type) {
+        if (event.target.files && event.target.files[0]) {
+            var reader = new FileReader();
+            reader.readAsDataURL(event.target.files[0]);
+            reader.onload = () => {
+                if (type === 'checkin') {
+                    this.checkinImage = reader.result;
+                } else {
+                    this.checkoutImage = reader.result;
+                }
+            }
+        }
+    }
+
+    chooseImage(imageEl: HTMLInputElement){
+        imageEl.click();
+    }
 }
